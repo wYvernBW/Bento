1e5d880cad894404e0045943a6bb133967735665
wyvern <50150016+wYvernBW@users.noreply.github.com>
2021-01-01 13:55:30 +0200

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ var today = new Date();
 var hour = today.getHours();
 
 // Here you can change your name
-var name = 'Colin';
+var name = 'Calin';
 
 // Here you can change your greetings
 var gree1 = 'Go to Sleep! ';
